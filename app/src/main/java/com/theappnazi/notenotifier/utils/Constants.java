6a802a911d8a6e7649b5bfa2b08b8330391996ae
Visesh <viseshrprasad@gmail.com>
2016-12-21 19:29:00 -0500

    add constants

---
+++ app/src/main/java/com/theappnazi/notenotifier/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.theappnazi.notenotifier.utils;
+
+/**
+ * Created by viseshprasad on 11/9/16.
+ */
+
+public class Constants {
+    public static final String USER_SETTINGS = "preference";
+    public static final String APP_SETTINGS = "app_preference";
+}
